fab25ef50c8c7c8b45c65f7633d831df01e9c54f
Albom <albom85@yandex.ru>
2018-03-02 10:56:56 +0200

    Header removed

--- geom.py
+++ geom.py
@@ -24,13 +24,13 @@ def geom(lat, long, year):
 
     headers = {
         'Connection': 'close',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
     }
 
     url = 'https://omniweb.gsfc.nasa.gov/cgi/vitmo/vitmo_model.cgi'
 
     r = requests.post(url, data=data, proxies=proxies, headers=headers)
-    ss = r.text[r.text.index('<pre>')+5: r.text.index('</pre>')].strip().split('\n')[5].strip().split(' ')
+    ss = r.text[r.text.index('<pre>')+5: r.text.index('</pre>')]
+    ss = ss.strip().split('\n')[5].strip().split(' ')
     return (float(ss[0]), float(ss[-1]))
 
 
