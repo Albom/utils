25bf5cf4a417f7cde888fc84b1021b94afce5544
Albom <o.v.bogomaz1985@gmail.com>
2019-09-15 16:29:00 +0300

    Loading files from FTP servers

--- ftp.py
+++ ftp.py
@@ -11,8 +11,14 @@ class Ftp:
         MODES = set([FILES_ONLY, DIRECTORIES_ONLY, ALL])
 
     def __init__(self, proxy=None):
-        self.proxy = proxy
         self.curl = pycurl.Curl()
+        if proxy:
+            delim = proxy.rfind(':')
+            proxy_url, proxy_port = proxy[:delim], proxy[delim+1:]
+            proxy_port = int(proxy_port)
+            self.curl.setopt(pycurl.PROXY, proxy_url)
+            self.curl.setopt(pycurl.PROXYPORT, proxy_port)
+            self.curl.setopt(pycurl.HTTPPROXYTUNNEL, 1)
 
     def list(
              self,
@@ -21,13 +27,6 @@ class Ftp:
              mode = ListMode.ALL
             ):
         self.curl.setopt(pycurl.URL, url)
-        if self.proxy:
-            delim = self.proxy.rfind(':')
-            proxy_url, proxy_port = self.proxy[:delim], self.proxy[delim+1:]
-            proxy_port = int(proxy_port)
-            self.curl.setopt(pycurl.PROXY, proxy_url)
-            self.curl.setopt(pycurl.PROXYPORT, proxy_port)
-            self.curl.setopt(pycurl.HTTPPROXYTUNNEL, 1)
         output = BytesIO()
         self.curl.setopt(pycurl.WRITEFUNCTION, output.write)
         self.curl.perform()
@@ -36,28 +35,41 @@ class Ftp:
                      filter(lambda x: len(x) > 0, result.split('\n'))))
 
         result = []
-
         for line in lines:
             columns = line.split()
-            if columns:
-                name = columns[-1]
-                isdir = columns[0].startswith('d')
-                s = line if raw else name
-                if (mode == self.ListMode.DIRECTORIES_ONLY) and isdir:
-                    result.append(s)
-                elif (mode == self.ListMode.FILES_ONLY) and not isdir:
-                    result.append(s)
-                elif mode == self.ListMode.ALL:
-                    result.append(s)
-                elif mode not in self.ListMode.MODES:
-                    format = 'ListMode value: {}. Should be {}'
-                    message = format.format(mode, self.ListMode.MODES)
-                    raise ValueError(message)
+            name = columns[-1]
+            isdir = columns[0].startswith('d')
+            s = line if raw else name
+            if (mode == self.ListMode.DIRECTORIES_ONLY) and isdir:
+                result.append(s)
+            elif (mode == self.ListMode.FILES_ONLY) and not isdir:
+                result.append(s)
+            elif mode == self.ListMode.ALL:
+                result.append(s)
+            elif mode not in self.ListMode.MODES:
+                format = 'ListMode value: {}. Should be {}'
+                message = format.format(mode, self.ListMode.MODES)
+                raise ValueError(message)
 
         return result
+        
+    def download(self, url):
+        self.curl.setopt(pycurl.URL, url)
+
+        output = BytesIO()
+        self.curl.setopt(pycurl.WRITEFUNCTION, output.write)
+        self.curl.perform()
+        return output.getvalue()
 
 if __name__ == '__main__':
     ftp = Ftp()
+ 
     print(ftp.list('ftp://ftp.aiub.unibe.ch/CODE/',
                    raw=False,
                    mode=Ftp.ListMode.DIRECTORIES_ONLY))
+
+
+    with open('COPG2570.19I.Z', 'wb') as file:
+        file.write(ftp.download('ftp://ftp.aiub.unibe.ch/CODE/COPG2570.19I.Z'))
+
+
